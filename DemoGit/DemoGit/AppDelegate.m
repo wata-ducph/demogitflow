c0018501b65e028e65bc084d8336bc40ff42a8bc
Tien Nguyen <thanhtien2302@gmail.com>
2016-05-04 22:22:42 +0700

    Tien's commit

--- DemoGit/DemoGit/AppDelegate.m
+++ DemoGit/DemoGit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // Hello git
     return YES;
 }
 
--- DemoGit/DemoGit/ViewController.h
+++ DemoGit/DemoGit/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+// Hello git
 
 @end
 
