d898576e04710435ae6400a1bd0f2f43b070214a
DucPV5 <ducpv5@311DucPV5-MC.local>
2016-05-04 22:16:20 +0700

    first commit

---
+++ DemoGit/DemoGit/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  DemoGit
+//
+//  Created by DucPV5 on 5/4/16.
+//  Copyright © 2016 ddd69. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ DemoGit/DemoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGit
+//
+//  Created by DucPV5 on 5/4/16.
+//  Copyright © 2016 ddd69. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
